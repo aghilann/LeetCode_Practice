6c0fae3da3350203fe7ab987f75b2554a92c2aa0
aghilann <90127404+aghilann@users.noreply.github.com>
2022-02-01 15:12:37 -0800

    Update ThirdMaximumNumber.py

--- Easy/ThirdMaximumNumber.py
+++ Easy/ThirdMaximumNumber.py
@@ -1,32 +1,9 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-#         max_1 = max(nums)
-#         arr1 = list(filter(lambda n: max_1 != n, nums))
+        x = sorted(list(set(nums)), reverse=True)
         
-#         if arr1 == []:
-#             return max_1
-        
-#         max_2 = max(arr1)
-#         arr2 = list(filter(lambda n: max_2 != n, arr1))
-        
-#         if arr2 == []:
-#             return max_1
-        
-#         return max(arr2)
-
-        
-        nums = list(set(nums))
-        max1 = max(nums)
-    
-        nums.remove(max1)
-        
-        if nums == []:
-            return max1
-    
-        max2 = max(nums)
-        nums.remove(max2)
-        
-        if nums == []:
-            return max1
-        
-        return max(nums)
+        if len(x) < 3:
+            return x[0]
+        else:
+            return x[2]
+            
