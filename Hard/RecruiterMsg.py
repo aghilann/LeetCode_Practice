8abf21cc275b414a1ff4b8acba816398ccb88f0b
Aghilan Nathan <90127404+aghilann@users.noreply.github.com>
2022-07-28 23:40:03 -0700

    Update RecruiterMsg.py

--- Hard/RecruiterMsg.py
+++ Hard/RecruiterMsg.py
@@ -1,11 +1,19 @@
-name = ""
-roleType = "Software"
-jobID = ""
+
+details = {
+  "name": "Taylor",
+  "roleType": "Software",
+  "jobID": "Software Engineer - San Francisco",
+  "companyName": "Asana"
+}
+
+title = f"Questions regarding Software Engineering Internships at {details['companyName']} !"
 
 msg = f"""
-“Hi <RecruiterName>,  
+“Hi {details["name"]},  
 
-I hope you’re well! I noticed you have posted job openings about ____ roles on LinkedIn and I’d like to express my candidacy for this role. I have already applied/am planning to apply to <JobID> and would love if you are able to review my application or if you have time for a coffee chat to discuss the role and expectations and any tips you might have. I was wondering does <X> sponsor Canadians for internships. I also wanted to get your advise on how Canadians can break into US tech industry.
+I hope you’re well! I noticed you have posted job openings for {details["roleType"]} roles on LinkedIn and I’d like to express my candidacy for this role. I have already applied/am planning to apply to {details["jobID"]} and would love if you are able to review my application or if you have time for a coffee chat to discuss the role and expectations and any tips you might have. I was wondering does {details["companyName"]} sponsor Canadians for internships. I also wanted to get your advise on how Canadians can break into US tech industry.
 
 Have a great rest of your week.” 
 """
+print(title)
+print(msg)
