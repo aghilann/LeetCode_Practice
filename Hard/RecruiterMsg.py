7ad65925884b94345e99327c717b336649a41643
Aghilan Nathan <90127404+aghilann@users.noreply.github.com>
2022-07-28 23:28:26 -0700

    Create RecruiterMsg.py

---
+++ Hard/RecruiterMsg.py
@@ -0,0 +1,11 @@
+name = ""
+roleType = "Software"
+jobID = ""
+
+msg = f"""
+“Hi <RecruiterName>,  
+
+I hope you’re well! I noticed you have posted job openings about ____ roles on LinkedIn and I’d like to express my candidacy for this role. I have already applied/am planning to apply to <JobID> and would love if you are able to review my application or if you have time for a coffee chat to discuss the role and expectations and any tips you might have. I was wondering does <X> sponsor Canadians for internships. I also wanted to get your advise on how Canadians can break into US tech industry.
+
+Have a great rest of your week.” 
+"""
